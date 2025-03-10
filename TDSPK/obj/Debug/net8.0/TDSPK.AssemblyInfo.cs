e0521d0d0025d51ab0ce2fa8a2936eadc5243278
Vinicius Saes <viniciusaesouza@gmail.com>
2025-03-10 17:53:52 -0300

    fix: refactor appsettings

--- TDSPK/obj/Debug/net8.0/TDSPK.AssemblyInfo.cs
+++ TDSPK/obj/Debug/net8.0/TDSPK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDSPK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c8b983a27a966d40a8ad352d9b99d145cea121")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b1ebbdc54f913256ec5deec20655b107adb429")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDSPK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDSPK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
